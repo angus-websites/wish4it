7a6041a404c54bf45fc351b7ce331ffc4d86921d
Angus <angus.goody@gmail.com>
2023-07-09 20:40:43 +0100

    Adding TextArea component

---
+++ resources/js/Components/form/TextArea.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { onMounted, ref } from 'vue';
+
+defineProps({
+    modelValue: String,
+    label: String
+});
+
+defineEmits(['update:modelValue']);
+
+const input = ref(null);
+
+onMounted(() => {
+    if (input.value.hasAttribute('autofocus')) {
+        input.value.focus();
+    }
+});
+
+defineExpose({ focus: () => input.value.focus() });
+</script>
+
+<template>
+  <div>
+    <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{label}}</label>
+    <div class="mt-2">
+      <textarea 
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+      ref="input"
+      rows="4" 
+      name="comment" 
+      id="comment" 
+      class="block p-2.5 w-full border-light-dark2 dark:border-dark-light2 dark:bg-gray-900 dark:text-gray-300 focus:border-primary-dark dark:focus:border-primary-light focus:ring-primary-dark dark:focus:ring-primary-light2 rounded-md shadow-sm" />
+    </div>
+  </div>
+</template>
+
--- resources/js/Components/wishlist/NewItemModal.vue
+++ resources/js/Components/wishlist/NewItemModal.vue
@@ -8,22 +8,90 @@
       <div class="fixed inset-0 z-10 overflow-y-auto">
         <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
           <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" enter-to="opacity-100 translate-y-0 sm:scale-100" leave="ease-in duration-200" leave-from="opacity-100 translate-y-0 sm:scale-100" leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
-            <DialogPanel class="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
-              <div>
-                <div class="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-green-100">
-                  <CheckIcon class="h-6 w-6 text-green-600" aria-hidden="true" />
+            <DialogPanel class="relative transform overflow-hidden rounded-lg bg-white dark:bg-dark px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
+
+              <!-- Start screen -->
+              <div v-if="!showDetails">
+
+                <!-- URL link -->
+                <div class="mb-6">
+                    <InputLabel for="url" value="Enter a product url" />
+                    <TextInput
+                        id="url"
+                        type="url"
+                        class="mt-1 block w-full"
+                        required
+                        autofocus
+                    />
                 </div>
-                <div class="mt-3 text-center sm:mt-5">
-                  <DialogTitle as="h3" class="text-base font-semibold leading-6 text-gray-900">Payment successful</DialogTitle>
-                  <div class="mt-2">
-                    <p class="text-sm text-gray-500">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius aliquam laudantium explicabo pariatur iste dolorem animi vitae error totam. At sapiente aliquam accusamus facere veritatis.</p>
+
+                <!-- Manual button -->
+                <div class="mb-6 text-center">
+                  <p class="text-gray-200"> Or </p>
+                  <button @click="showDetails = true" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Enter manually</button>
+                </div>
+
+                <div class="mt-5 text-center">
+                  <SecondaryButton class="w-full" @click="closeModal">Cancel</SecondaryButton>
+                </div>
+
+              </div>
+
+              <!-- Details screen -->
+              <div v-else>
+                
+                <!-- Form elements -->
+                <div class="flex flex-col gap-y-3">
+
+                  <!-- Product name -->
+                  <div>
+                      <InputLabel for="email" value="Name of product" />
+                      <TextInput
+                          id="email"
+                          type="email"
+                          class="w-full mt-1"
+                          required
+                          autofocus
+                      />
                   </div>
+
+                  <!-- Brand-->
+                  <div>
+                      <InputLabel for="brand" value="Brand" />
+                      <TextInput
+                          id="brand"
+                          type="text"
+                          class="w-full mt-1"
+                      />
+                  </div>
+
+                  <!-- Price-->
+                  <div>
+                      <InputLabel for="price" value="Price (£)" />
+                      <TextInput
+                          id="price"
+                          type="number"
+                          class="w-full mt-1"
+                          placeholder="5"
+                      />
+                  </div>
+
+                  <!-- Comments -->
+                  <TextArea label="Comments" />
+                  
                 </div>
+
+                <!-- Buttons -->
+                <div class="mt-5 flex flex-col gap-y-3 sm:flex-row gap-x-3">
+                  <SecondaryButton>Cancel</SecondaryButton>
+                  <PrimaryButton>Save</PrimaryButton>
+                </div>
+
+
+
               </div>
-              <div class="mt-5 sm:mt-6 sm:grid sm:grid-flow-row-dense sm:grid-cols-2 sm:gap-3">
-                <button type="button" class="inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 sm:col-start-2" @click="closeModal">Deactivate</button>
-                <button type="button" class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:col-start-1 sm:mt-0" @click="closeModal" ref="cancelButtonRef">Cancel</button>
-              </div>
+
+              
             </DialogPanel>
           </TransitionChild>
         </div>
@@ -33,24 +101,35 @@
 </template>
 
 <script setup>
-import { ref, watchEffect } from 'vue'
+import { ref, watchEffect, onBeforeUpdate } from 'vue'
 import { Dialog, DialogPanel, DialogTitle, TransitionChild, TransitionRoot } from '@headlessui/vue'
 import { CheckIcon } from '@heroicons/vue/24/outline'
+import PrimaryButton from "@/Components/buttons/PrimaryButton.vue"
+import SecondaryButton from "@/Components/buttons/SecondaryButton.vue"
+import InputLabel from "@/Components/form/InputLabel.vue"
+import TextInput from "@/Components/form/TextInput.vue"
+import TextArea from "@/Components/form/TextArea.vue"
 
 const props = defineProps({
     open: Boolean,
+
 })
 
 let isOpen = ref(props.open)
+let showDetails = ref(false)
+
+const emit = defineEmits(['update:open'])
+
 
 watchEffect(() => {
     isOpen.value = props.open
 })
 
-const emit = defineEmits(['update:open'])
 
 function closeModal() {
-    emit('update:open', false)
+  // Hide the details screen and close the modal
+  emit('update:open', false)
+  showDetails.value = false;
 }
 
 </script>
