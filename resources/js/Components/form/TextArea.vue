599a2e995e0f37d8d7b71639eb4375ddbd50ff75
Angus <angus.goody@gmail.com>
2023-07-11 19:54:49 +0100

    Added TextArea component

--- resources/js/Components/form/TextArea.vue
+++ resources/js/Components/form/TextArea.vue
@@ -3,7 +3,9 @@ import { onMounted, ref } from 'vue';
 
 defineProps({
     modelValue: String,
-    label: String
+    label: String,
+    id: String,
+    name: String
 });
 
 defineEmits(['update:modelValue']);
@@ -21,15 +23,15 @@ defineExpose({ focus: () => input.value.focus() });
 
 <template>
   <div>
-    <label for="message" class="block mb-2 text-sm font-medium text-gray-700 dark:text-gray-300">{{label}}</label>
+    <label :for="id" class="block mb-2 text-sm font-medium text-gray-700 dark:text-gray-300">{{label}}</label>
     <div class="mt-2">
       <textarea 
       :value="modelValue"
       @input="$emit('update:modelValue', $event.target.value)"
+      :id="id"
       ref="input"
       rows="4" 
-      name="comment" 
-      id="comment" 
+      :name="name" 
       class="block p-2.5 w-full border-light-dark2 dark:border-dark-light2 dark:bg-gray-900 dark:text-gray-300 focus:border-primary-dark dark:focus:border-primary-light focus:ring-primary-dark dark:focus:ring-primary-light2 rounded-md shadow-sm" />
     </div>
   </div>
