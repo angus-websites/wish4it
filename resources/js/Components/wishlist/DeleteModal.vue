91db55c3fb89aa64f0bca2b6a1ef563e80b57cba
Angus <angus.goody@gmail.com>
2023-07-12 16:05:16 +0100

    Added better delete modal ui

--- resources/js/Components/wishlist/DeleteModal.vue
+++ resources/js/Components/wishlist/DeleteModal.vue
@@ -9,24 +9,20 @@
         <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
           <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" enter-to="opacity-100 translate-y-0 sm:scale-100" leave="ease-in duration-200" leave-from="opacity-100 translate-y-0 sm:scale-100" leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
             <DialogPanel class="relative transform overflow-hidden rounded-lg bg-white dark:bg-dark px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
+
               <div class="relative w-full max-w-md max-h-full">
-                  <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                      <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="popup-modal">
-                          <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
-                          </svg>
-                          <span class="sr-only">Close modal</span>
-                      </button>
-                      <div class="p-6 text-center">
-                          <svg class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
-                          </svg>
-                          <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to delete this product?</h3>
-                          <button data-modal-hide="popup-modal" type="button" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
-                              Yes, I'm sure
-                          </button>
-                          <button data-modal-hide="popup-modal" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">No, cancel</button>
+                  <div class="p-6 text-center">
+                      <svg class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
+                      </svg>
+                      <p class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-200">Are you sure you want to delete this product?</p>
+
+                      <div class="mt-5 flex flex-col gap-y-3 sm:flex-row gap-x-3 justify-center">
+                        <DangerButton>Delete</DangerButton>
+                        <SecondaryButton @click="closeModal" type="button">Cancel</SecondaryButton>
                       </div>
+
+                      
                   </div>
               </div>
             </DialogPanel>
@@ -40,13 +36,8 @@
 <script setup>
 import { ref, watchEffect, onBeforeUpdate } from 'vue'
 import { Dialog, DialogPanel, DialogTitle, TransitionChild, TransitionRoot } from '@headlessui/vue'
-import { CheckIcon } from '@heroicons/vue/24/outline'
-import PrimaryButton from "@/Components/buttons/PrimaryButton.vue"
+import DangerButton from "@/Components/buttons/DangerButton.vue"
 import SecondaryButton from "@/Components/buttons/SecondaryButton.vue"
-import InputLabel from "@/Components/form/InputLabel.vue"
-import TextInput from "@/Components/form/TextInput.vue"
-import TextArea from "@/Components/form/TextArea.vue"
-import InputError from "@/Components/form/InputError.vue"
 
 import {useForm} from '@inertiajs/vue3';
 
