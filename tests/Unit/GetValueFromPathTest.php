658660687501c578567e26410da248158026cd28
Angus <angus.goody@gmail.com>
2023-07-20 16:47:49 +0100

    Added tests

--- tests/Unit/GetValueFromPathTest.php
+++ tests/Unit/GetValueFromPathTest.php
@@ -35,8 +35,125 @@ class GetValueFromPathTest extends TestCase
 
         $scraper = new JsonLdScraper(null);
 
-        $name = $scraper->getValueFromPath($jsonLdData, ["brand", "name"]);
-        $this->assertEquals("Test Brand", $name);
+        $brand = $scraper->getValueFromPath($jsonLdData, ["brand", "name"]);
+        $this->assertEquals("Test Brand", $brand);
+
+    }
+
+    public function testNormalPrice(): void
+    {
+        $jsonLdData = [
+            "name" => "Test Product",
+            "price" => "30.00"
+        ];
+
+        $scraper = new JsonLdScraper(null);
+
+        $price = $scraper->getValueFromPath($jsonLdData, ["price"]);
+        $this->assertEquals("30.00", $price);
+
+    }
+
+    public function testPriceInOffers(): void
+    {
+        $jsonLdData = [
+            "name" => "Test Product",
+            "offers" => [
+                "@type" => "Offer",
+                "price" => "12.00",
+                "priceCurrency" => "GBP",
+            ]
+        ];
+
+        $scraper = new JsonLdScraper(null);
+
+        $price = $scraper->getValueFromPath($jsonLdData, ["offers", "price"]);
+        $this->assertEquals("12.00", $price);
+
+    }
+
+    public function testPriceInNestedOffers(): void
+    {
+        $jsonLdData = [
+            "name" => "Test Product",
+            "offers" => [
+                "@type" => "OfferParent",
+                "priceCurrency" => "GBP",
+                "offers" => [
+                    "@type" => "Offer",
+                    "price" => "15.00",
+                    "priceCurrency" => "GBP",
+                ]
+            ]
+        ];
+
+        $scraper = new JsonLdScraper(null);
+
+        $price = $scraper->getValueFromPath($jsonLdData, ["offers", "*", "price"]);
+        $this->assertEquals("15.00", $price);
+
+    }
+
+    public function testPriceInNestedOffersArray(): void
+    {
+        $jsonLdData = [
+            "name" => "Test Product",
+            "offers" => [
+                "@type" => "AggregateOffer",
+                "priceCurrency" => "GBP",
+                "offerCount" => 2,
+                "offers" => [
+                    [
+                        "@type" => "Offer",
+                        "price" => "15.00",
+                        "priceCurrency" => "GBP",
+                    ],
+                    [
+                        "@type" => "Offer",
+                        "price" => "38.00",
+                        "priceCurrency" => "GBP",
+                    ],
+                ]
+            ]
+        ];
+
+        $scraper = new JsonLdScraper(null);
+
+        $price = $scraper->getValueFromPath($jsonLdData, ["offers", "*", "price"]);
+        $this->assertEquals("15.00", $price);
+
+    }
+
+    public function testNormalImage(): void
+    {
+        $jsonLdData = [
+            "name" => "Test Product",
+            "image" => "test.jpg"
+        ];
+
+        $scraper = new JsonLdScraper(null);
+
+        $image = $scraper->getValueFromPath($jsonLdData, ["image"]);
+        $this->assertEquals("test.jpg", $image);
+
+    }
+
+    public function testNestedImage(): void
+    {
+        $jsonLdData = [
+            "name" => "Test Product",
+            'image' => [
+                [
+                    '@type' => 'ImageObject',
+                    'thumbnail' => 'test.jpg',
+                ],
+            ],
+        ];
+
+        $scraper = new JsonLdScraper(null);
+
+        $image = $scraper->getValueFromPath($jsonLdData, ['image','*','thumbnail']);
+        $this->assertEquals("test.jpg", $image);
 
     }
 }
