d119111827ade12f3cf584119419f5a7441e8fa6
Angus <angus.goody@gmail.com>
2023-07-21 09:50:15 +0100

    Refactor controller

--- app/Http/Controllers/ProductScraperController.php
+++ app/Http/Controllers/ProductScraperController.php
@@ -5,21 +5,25 @@ namespace App\Http\Controllers;
 use App\Services\ProductScraperService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Client\RequestException;
+use Illuminate\Support\Facades\Validator;
+
 
 class ProductScraperController extends Controller
 {
-    protected $scraperService;
+    private ProductScraperService $scraperService;
 
     public function __construct(ProductScraperService $scraperService)
     {
         $this->scraperService = $scraperService;
     }
 
-    public function scrapeProduct(Request $request)
+    public function scrapeProduct(Request $request): JsonResponse
     {
-        // Ensure we have a url in the request
+
+        // Validate the input
         $validator = Validator::make($request->all(), [
             'url' => 'required|url',
         ]);
@@ -28,39 +32,36 @@ class ProductScraperController extends Controller
             return response()->json(['errors' => $validator->errors()], 400);
         }
 
+        // Extract the input from the request
         $url = $request->input('url');
 
-        // Use Laravel's HTTP client to get the HTML content
-        // Retry up to 2 times, with a wait time of 3 seconds before giving up
         try {
-            $response = retry(2, function () use ($url) {
-                return Http::timeout(3)->withHeaders([
-                    'User-Agent' => "MyWishlistApp lookup",
-                ])->get($url);
-            }, 1000);
+            $response = Http::timeout(3)->withHeaders([
+                'User-Agent' => "MyWishlistApp lookup",
+            ])->retry(2, 1000)->get($url);
 
-            // If the request was successful, continue processing...
             if ($response->successful()) {
                 $htmlContent = $response->body();
-
-                // Scrape the data using ProductScraperService
                 $product = $this->scraperService->scrapeProduct($htmlContent);
-
                 return response()->json(["product" => $product]);
             }
+            else {
 
-        } catch (\Exception $exception) {
+                Log::channel("scraper")->error('Could not receive html data for scraping', [
+                    'url' => $url,
+                    'status_code' => $response->status(),
+                ]);
+            }
 
-            Log::channel('scraper')->error('An error occurred during URL scraping', [
+        } catch (RequestException $exception) {
+            Log::channel("scraper")->error('An error occurred during URL scraping', [
                 'url' => $url,
                 'error_message' => $exception->getMessage(),
             ]);
-    
-            // If the request was not successful after retrying, return a response indicating the error
+
             return response()->json(['error' => 'Unable to scrape product at this time. Please try again later.'], 500);
         }
+
+        return response()->json(['error' => 'Unable to scrape this website'], 500);
     }
-
-
 }
-
