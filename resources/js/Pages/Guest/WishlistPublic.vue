57b6b2cc239a757a4a2cece2187c20ca81b33be6
Angus <angus.goody@gmail.com>
2023-08-13 16:50:10 +0100

    Adding view for public wishlist

---
+++ resources/js/Pages/Guest/WishlistPublic.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+import GuestLayout from "@/Layouts/GuestLayout.vue";
+import PageContainer from "@/Components/_util/PageContainer.vue";
+import PrimaryButton from "@/Components/buttons/PrimaryButton.vue";
+
+defineProps({
+    canLogin: Boolean,
+    canRegister: Boolean,
+    laravelVersion: String,
+    phpVersion: String,
+});
+</script>
+
+<template>
+    <GuestLayout title="Home">
+        <PageContainer>
+            <h1 class="h1">Wishlist</h1>
+        </PageContainer>
+    </GuestLayout>
+</template>
+
