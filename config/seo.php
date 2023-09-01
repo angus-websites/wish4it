91ed0ef3717da3cec7500f1589be51dddc6938c5
Angus <angus.goody@gmail.com>
2023-09-01 17:30:02 +0100

    updated blade SEO page

---
+++ config/seo.php
@@ -0,0 +1,8 @@
+<?php
+return [
+
+    // SEO details
+
+    'title' => env('APP_NAME', 'Laravel'),
+    'description' => 'Wish4it: Create the perfect wishlist for any occasion and effortlessly share it with family and friends. From birthdays and holidays to special events, Wish4it makes gifting a breeze.',
+];
