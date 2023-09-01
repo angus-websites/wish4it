3416441b7bf3b9864d52dff5bcaa32215a88ca29
Angus Goody <angus.goody@gmail.com>
2023-09-01 17:38:41 +0100

    Seo (#65)
    * Added new assets for SEO
    
    * updated blade SEO page
    
    * Added favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,16 +1,32 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+
+        <title inertia>{{config('seo.title') }}</title>
+        <link rel="icon" type="image/png" href="{{ asset('assets/images/core/favicon.png') }}">
+        <link rel="canonical" href="{{ url()->current() }}">
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="{{config('seo.description') }}">
+        <meta name="keywords" content="Wish4it, Wishlist, Gift registry, Share wishlists, Family gifts, Friend gifts, Event wishlists, Birthday gifts, Holiday wishes, Special occasion gifts">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <meta property="og:title" content="{{config('seo.title')}}">
+        <meta property="og:description" content="{{config('seo.description') }}">
+        <meta property="og:url" content="{{ url()->current() }}">
 
-        <!-- Favicon -->
-        <link rel="icon" type="image/png" href="{{ asset('assets/images/core/favicon.png') }}">
+        <meta property="og:image" content="{{ asset('assets/images/meta/ogimage.png') }}">
+        <meta property="og:image:type" content="image/jpeg">
+        <meta property="og:image:width" content="1200">
+        <meta property="og:image:height" content="630">
 
-        <!-- Scripts -->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.7.0/flowbite.min.js"></script>
+        <!-- Icons -->
+        @foreach ([16, 32, 48] as $size)
+            <link rel="icon" type="image/png" sizes="{{ $size }}x{{ $size }}" href="{{ asset("assets/images/meta/favicon-{$size}x{$size}.png") }}">
+        @endforeach
+
+        @foreach ([180, 152, 120] as $size)
+            <link rel="apple-touch-icon" sizes="{{ $size }}x{{ $size }}" href="{{ asset("assets/images/meta/apple-touch-icon-{$size}x{$size}.png") }}">
+        @endforeach
 
         @routes
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
