3bbf3161d4ba811dd695d673935af1f0c4e9108e
Angus <angus.goody@gmail.com>
2023-12-16 18:55:15 +0000

    added tests

--- tests/Feature/Http/Controllers/WishlistControllerTest.php
+++ tests/Feature/Http/Controllers/WishlistControllerTest.php
@@ -30,7 +30,7 @@ class WishlistControllerTest extends TestCase
     /**
      * Test the index page
      */
-    public function test_index_page()
+    public function test_index_page_for_normal_user()
     {
         $response = $this->actingAs($this->user)
             ->get(route('wishlists.index'));
@@ -41,4 +41,91 @@ class WishlistControllerTest extends TestCase
             ->has('lists')
         );
     }
+
+    /**
+     * Test the index page for a guest
+     */
+    public function test_index_page_for_guest()
+    {
+        $response = $this->get(route('wishlists.index'));
+
+        $response->assertStatus(302);
+        $response->assertRedirect(route('login'));
+    }
+
+    /**
+     * Test the store route
+     */
+    public function test_store_route_for_normal_user()
+    {
+        $response = $this->actingAs($this->user)
+            ->post(route('wishlists.store'), [
+                'title' => 'Test Wishlist',
+                'public' => true,
+            ]);
+
+        $response->assertStatus(302);
+        $response->assertSessionHas('success', 'Wishlist created');
+
+        // Assert that the wishlist was created
+        $this->assertDatabaseHas('wishlists', [
+            'title' => 'Test Wishlist',
+            'public' => true,
+        ]);
+    }
+
+    /**
+     * Test the store route for a guest
+     */
+    public function test_store_route_for_guest()
+    {
+        $response = $this->post(route('wishlists.store'), [
+            'title' => 'Test Wishlist',
+            'public' => true,
+        ]);
+
+        $response->assertStatus(302);
+        $response->assertRedirect(route('login'));
+
+        // Assert that the wishlist was not created
+        $this->assertDatabaseMissing('wishlists', [
+            'title' => 'Test Wishlist',
+            'public' => true,
+        ]);
+    }
+
+    /**
+     * Test store route without a title
+     */
+    public function test_store_route_validation()
+    {
+
+        $this->actingAs($this->user);
+
+        // Test without a title
+        $response = $this->post(route('wishlists.store'), [
+            'public' => true,
+        ]);
+
+
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors('title');
+
+        // Test without a public value
+        $response = $this->post(route('wishlists.store'), [
+            'title' => 'Test Wishlist',
+        ]);
+
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors('public');
+
+        // Test without any data
+        $response = $this->post(route('wishlists.store'));
+
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors(['title', 'public']);
+    }
+
+
+
 }
