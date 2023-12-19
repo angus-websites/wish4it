06df3e28f9d8df6cef7501eae1c6ced45f3c9b8b
Angus <angus.goody@gmail.com>
2023-12-19 00:22:28 +0000

    added performance tests

--- tests/Performance/WishlistPerformanceTest.php
+++ tests/Performance/WishlistPerformanceTest.php
@@ -7,20 +7,114 @@ use App\Models\User;
 use App\Models\Wishlist;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use Illuminate\Support\Benchmark;
 
 class WishlistPerformanceTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_show_wishlist(): void
+    protected function runBenchmark($userCount, $wishlistCount, $itemCount): void
     {
-        // Create 1000 users with 5 wishlists each, with 50 products each
-        User::factory()->count(1000)->create()->each(function ($u) {
+        // Create users
+        User::factory()->count($userCount)
+            ->hasAttached(
+                Wishlist::factory()
+                    ->count($wishlistCount)
+                    ->public(true)
+                    ->hasItems($itemCount),
+                ['role' => 'owner'],
+            )->create();
 
-            $wishlist = Wishlist::create();
-            $wishlist->users()->attach($u->id, ['role' => 'owner']);
+        // Create our test user
+        $user = User::factory()->create();
 
+        // Create a wishlist for our test user
+        $wishlist = Wishlist::factory()->create();
+        $wishlist->users()->attach($user->id, ['role' => 'owner']);
+
+        // Time how long it takes to load the first wishlist with benchmark
+        $time = Benchmark::measure(function () use ($user) {
+            $this->actingAs($user)
+                ->get(route('wishlists.show', $user->wishlists->first()));
         });
+
+
+        // Output the time taken in seconds and milliseconds leftover
+        $this->outputTime($time, 'load users only wishlist');
+
+        // Assert it takes less than 1 second
+        $this->assertTrue($time < 1000);
+
+        // Pick a random wishlist to view
+        $wishlist = Wishlist::all()->random();
+
+        // Time how long it takes to load a random wishlist with benchmark
+        $time = Benchmark::measure(function () use ($user, $wishlist) {
+            $this->actingAs($user)
+                ->get(route('wishlists.show', $wishlist));
+        }, 50);
+
+        // Output the time taken in seconds and milliseconds leftover
+        $this->outputTime($time, 'view random wishlist as user');
+
+        // Assert it takes less than 1 second
+        $this->assertTrue($time < 1000);
+
+        $wishlist = Wishlist::all()->random();
+
+        // Time how long it takes to load a random wishlist as guest with benchmark
+        $time = Benchmark::measure(function () use ($wishlist) {
+            $this->get(route('wishlists.show', $wishlist));
+        });
+
+        // Output the time taken in seconds and milliseconds leftover
+        $this->outputTime($time, 'view random wishlist as guest');
+
+        // Assert it takes less than 1 second
+        $this->assertTrue($time < 1000);
+        
+
     }
+
+    private function outputTime($time, $action): void
+    {
+        $seconds = floor($time / 1000);
+        $milliseconds = $time % 1000;
+        echo "\nTime taken to $action: $seconds seconds, $milliseconds milliseconds\n";
+    }
+
+
+    public function test_show_wishlist_with_fifteen_users(): void
+    {
+        // Takes around 4 seconds to generate 15 users
+        $this->runBenchmark(15, 5, 40);
+
+    }
+
+    public function test_show_wishlist_with_hundred_users(): void
+    {
+        // Takes around 24 seconds to generate 100 users
+        $this->runBenchmark(100, 5, 40);
+
+    }
+
+    public function test_show_wishlist_with_five_hundred_users(): void
+    {
+
+        // Takes around 2 minutes to generate 500 users
+        $this->runBenchmark(500, 10, 40);
+
+    }
+
+
+    public function test_show_wishlist_with_thousand_users(): void
+    {
+
+        // Takes around 8 minutes to generate 1000 users
+        $this->runBenchmark(1000, 10, 40);
+
+    }
+
+
 
 }
