aa3e8db2bef916bf1cab24364bdafd5acd5dccaf
Angus <angus.goody@gmail.com>
2024-12-08 21:24:42 +0000

    failing test

--- tests/Feature/Http/Resources/WishlistItemResourceTest.php
+++ tests/Feature/Http/Resources/WishlistItemResourceTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature\Http\Resources;
 use App\Http\Resources\WishlistItemResource;
 use App\Models\User;
 use App\Models\Wishlist;
+use App\Models\WishlistItem;
 use App\Services\WishlistService;
 use Database\Factories\WishlistItemFactory;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -126,6 +127,68 @@ class WishlistItemResourceTest extends TestCase
         ];
 
         $this->assertEquals($expected, $resource_array);
+    }
+
+    /**
+     * Test Linked Brands
+     */
+    public function test_wishlist_item_resource_with_linked_brands()
+    {
+        // Mock current datetime now
+        Carbon::setTestNow(
+            Carbon::create(2021, 1, 1, 12, 0, 0)
+        );
+
+        // Create a wishlist
+        $wishlist = Wishlist::factory()->public(true)->forUser($this->user)->create();
+
+        // Create two items using the factory with the same brand
+        $ipad = WishlistItem::factory()->create(
+             [
+                'wishlist_id' => $wishlist->id,
+                'needs' => 1,
+                'name' => 'ipad',
+                'brand' => 'apple',
+
+             ]
+        );
+
+        $mac = WishlistItem::factory()->create(
+             [
+                'wishlist_id' => $wishlist->id,
+                'needs' => 1,
+                'name' => 'mac',
+                'brand' => 'apple',
+
+             ]
+        );
+
+        // Pass to the resource
+        $resource = new WishlistItemResource($ipad);
+
+        $resource_array = $resource->toArray(request()->setUserResolver(fn () => $this->user));
+
+        // Assert that the structure is correct
+        $expected = [
+            'id' => $ipad->id,
+            'wishlist_id' => $ipad->wishlist_id,
+            'needs' => $ipad->needs,
+            'name' => $ipad->name,
+            'brand' => $ipad->brand,
+            'price' => $ipad->price,
+            'url' => $ipad->url,
+            'comment' => $ipad->comment,
+            'has' => 0,
+            'created_at' => '2021-01-01',
+            'image' => null,
+            'hasCurrentUserReservation' => new MissingValue,
+            'linkedShops' => false,
+            'linkedBrands' => true
+        ];
+
+        $this->assertEquals($expected, $resource_array);
+
+
     }
 
 
